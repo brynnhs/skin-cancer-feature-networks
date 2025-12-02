a6cdb36401c9a191ba99f5c97ddef1ada5ecf160
Brynn Harris-Shanks <brynn.hs@dal.ca>
2025-12-02 16:28:55 +0100

    requirements + folder structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# skin-cancer-feature-networks
+# Skin Cancer Feature Networks
+
+Analysis of feature networks in skin cancer data using Mixed Graphical Models (MGM) and causal inference methods.
---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+"""
+Skin Cancer Feature Networks Analysis
+
+A project for analyzing feature networks in skin cancer data using
+Mixed Graphical Models (MGM) and causal inference methods.
+
+This package contains modules for:
+- Data loading and preprocessing
+- Mixed-type encoding
+- MGM network estimation
+- Causal structure learning (PC-Stable, GES, NOTEARS)
+- Bootstrap analysis
+- Network metrics and comparisons
+- Visualization
+"""
